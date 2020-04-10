dc95e880c34c0c694114d94cc01082a26eee3b17
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-10 03:24:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,5 +129,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-		programmer_hash.push[:dennis_ritchie][:languages]"Assembly"
+		programmer_hash.push[:dennis_ritchie][:languages] = "Assembly"
 end
